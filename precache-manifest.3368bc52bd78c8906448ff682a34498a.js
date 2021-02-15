f26c91deea93f921d021f54e57a4c0d0d43b8dd3
Devansh Shukla <18ucs078@lnmiit.ac.in>
2021-02-15 16:31:14 +0530

    Updates

---
+++ precache-manifest.3368bc52bd78c8906448ff682a34498a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62c809902748b1b3611c0e4cbef9a5f2",
+    "url": "/Confusion-Restaurant-React-/index.html"
+  },
+  {
+    "revision": "73c9cc9af555a9baa654",
+    "url": "/Confusion-Restaurant-React-/static/css/2.a5b62288.chunk.css"
+  },
+  {
+    "revision": "d96904ec937bfee06a99",
+    "url": "/Confusion-Restaurant-React-/static/css/main.17322a8a.chunk.css"
+  },
+  {
+    "revision": "73c9cc9af555a9baa654",
+    "url": "/Confusion-Restaurant-React-/static/js/2.b36da100.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/Confusion-Restaurant-React-/static/js/2.b36da100.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d96904ec937bfee06a99",
+    "url": "/Confusion-Restaurant-React-/static/js/main.e20ddbf4.chunk.js"
+  },
+  {
+    "revision": "31a43994985882d5971b",
+    "url": "/Confusion-Restaurant-React-/static/js/runtime-main.eb0428f3.js"
+  }
+]);
